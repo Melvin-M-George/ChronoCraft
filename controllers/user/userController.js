b6945a6c442c866e868aab8c867e1bd0796e14a4
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-11-27 20:21:41 +0530

    Home page cutomisation

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -25,7 +25,7 @@ const loadHomepage = async (req, res) => {
 
 
         productData.sort((a,b)=>new Date(b.createdOn)-new Date(a.createdOn));
-        productData = productData.slice(0);
+        productData = productData.slice(8);
 
 
 
