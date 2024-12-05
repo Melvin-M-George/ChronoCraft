167620ca94f50374550ce6861f0a9d7ef6c243c5
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-12-05 17:13:26 +0530

    Cancelled and returned orders removed from sales report

--- controllers/admin/salesController.js
+++ controllers/admin/salesController.js
@@ -3,26 +3,35 @@ const User = require("../../models/userSchema");
 
 
 
-const getSalesReport = async (req,res) => {
+const getSalesReport = async (req, res) => {
     try {
-        
         const page = parseInt(req.query.page) || 1;
         const limit = 20;
-        const skip = (page-1) * limit;
-        const count = await Order.countDocuments();
-        const totalPages = Math.ceil(count / limit);
-        
-        
-        const orderData = await Order.find().populate("user").populate("orderedItems.product").sort({createdOn:-1}).skip(skip).limit(limit);
+        const skip = (page - 1) * limit;
 
-        
-        res.render("salesreport",{orders:orderData,count,totalPages,currentPage:page});
-        
+        // Exclude orders with status "Cancelled" or "Returned"
+        const filterOrders = { status: { $nin: ["Cancelled", "Returned"] } };
+        const count = await Order.countDocuments(filterOrders);
+        const totalPages = Math.ceil(count / limit);
+
+        const orderData = await Order.find(filterOrders)
+            .populate("user")
+            .populate("orderedItems.product")
+            .sort({ createdOn: -1 })
+            .skip(skip)
+            .limit(limit);
+
+        res.render("salesreport", {
+            orders: orderData,
+            count,
+            totalPages,
+            currentPage: page,
+        });
     } catch (error) {
-        console.error("Error loading sales report",error);
+        console.error("Error loading sales report", error);
         res.redirect("/pageerror");
     }
-}
+};
 
 
 
