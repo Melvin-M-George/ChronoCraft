a5527af0e066ba518302cc1fb22be2f585c2a450
Melvin-M-George <melvinmgeorge1234@gmail.com>
2024-12-11 20:21:05 +0530

    comments added

--- controllers/user/cartController.js
+++ controllers/user/cartController.js
@@ -133,7 +133,7 @@ const updateCartQuantity = async (req, res) => {
         const userId = req.session.user;
         
 
-       
+       //finding cart
         const cart = await Cart.findOne({ userId });
         if (!cart) return res.status(404).json({ success: false, message: "Cart not found" });
 
